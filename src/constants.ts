0b8fcaa625d9feae8cb039d4de1709d8eb598a09
Tuo Nome <double.facessss@gmail.com>
2024-04-18 20:59:26 +0200

    testing new component

--- src/constants.ts
+++ src/constants.ts
@@ -22,10 +22,14 @@ export const Constants = {
 	userMsgPrefix: userImgDiv + '<p class="userMsg">',
 	responseSuffix: '</p><div class="clearfix"></div>',
 	botTyping: botImgDiv + '<div class="botTyping">' + '<div class="bounce1"></div>' + '<div class="bounce2"></div>' + '<div class="bounce3"></div>' + '</div>'
-    */
+    */    
 }
 
-export const NewConstants = {
+
+
+
+
+export const Icons = {
     chatIcon: `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
             <path d="M2 15V5c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v15a1 1 0 0 1-1.7.7L16.58 17H4a2 2 0 0 1-2-2z"/>
@@ -35,6 +39,20 @@ export const NewConstants = {
     closeIcon: `
         <svg xmlns="http://www.w3.org/2000/svg" style="display: block; margin: auto; transform: scale(1.5, 1.5);" viewBox="0 0 24 24" class="icon">
              <path transform="translate(0.5,0)" fill-rule="evenodd" d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"/>
+        </svg>
+    `
+}
+
+export const NewIcons = {
+    chatIcon: `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
+            <path d="M2 15V5c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v15a1 1 0 0 1-1.7.7L16.58 17H4a2 2 0 0 1-2-2z"/>
+            <path  d="M6 7h12a1 1 0 0 1 0 2H6a1 1 0 1 1 0-2zm0 4h8a1 1 0 0 1 0 2H6a1 1 0 0 1 0-2z"/>
+        </svg>
+    `,
+    closeIcon: `
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: block; margin: auto; transform: scale(1.5, 1.5);" viewBox="0 0 24 24" class="icon">
+            <path transform="translate(0.5,0)" fill-rule="evenodd" d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"/>
         </svg>
     `
 }
--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,82 @@
 import { createTemplate } from './template'
 import {test} from './main'
+import {Constants} from './constants'
 import * as starter from './main'
 
-test('hello world!')
+//test('hello world!')
 
-/* @@ costants @@ */
-
-const WC_TAG_NAME = 'my-widget'
-
+/*
 declare const NuvolarisAssistant: any
 
 /* @@ init function @@ */
 
-function init() {
-    const widget = NuvolarisAssistant('Enzonav is Great Programmer!!')
+/*function init() {
+    const widget = NuvolarisAssistant()
 
     setTimeout(() => {
-        widget.greeting = 'This is another message!'
+      widget.greeting = 'This is another message!'
     }, 3000)
 }
 
 /* @@ start on load @@ */
 
-window.onload = init
+
 
 /* @@ core library @@ */
 
-export default function createComponent(greeting: string) {
-  const template = createTemplate(greeting)
+export default function createComponent() {
 
-  class GreeterWidgetElement extends HTMLElement {
+  class ChatBot extends HTMLElement {
+
+    private isOpen: boolean = false
+    private messages: { role: string; content: string }[]
+    private thinkingTimeout: NodeJS.Timeout | null 
+    private apiPending: boolean
+    private totalTokens: number
+    private styles: string  
+
     constructor() {
+
       super()
-  
-      const shadowDOM = this.attachShadow({ mode: 'open' })
-      // Render the template in the shadow dom
-      shadowDOM.appendChild(template.content.cloneNode(true))
+
+      this.attachShadow({ mode: "open" })
+      this.isOpen = false 
+      this.messages = [] 
+      this.thinkingTimeout = null 
+      this.apiPending = false 
+      this.totalTokens = 0
+      this.styles = ''
+
+      fetch('./style.css')  
+      .then(response => response.text())
+      .then(css => {        
+        this.styles = css        
+        this.render()
+        console.log('css loaded')
+        //this.addEventListeners()
+      })
+      .catch(e => console.log('error fetching style.css'))
+
+      console.log('web components runs!')
+      
+    }    
+
+    connectedCallback() {
+      this.render()
+      this.addEventListeners()
+    }
+    get test() {
+      return this.shadowRoot
     }
 
-    get greeting(): string {
-      const greetingEl = this.shadowRoot?.querySelector(
-        '.greeting'
-      )
-
-      return greetingEl?.textContent || '';
+    render(): void {
+      
     }
+    addEventListeners(): void {
 
-    set greeting(val: string) {
-      const greetingEl = this.shadowRoot?.querySelector(
-        '.greeting'
-      );
-
-      if (greetingEl) {
-        greetingEl.textContent = val;  
-      }
     }
   }
-
-  if (!customElements.get(WC_TAG_NAME)) {
-    customElements.define(WC_TAG_NAME, GreeterWidgetElement);
-  }
-
-  // create an instance of the component
-  const componentInstance = document.createElement(WC_TAG_NAME, {
-    is: WC_TAG_NAME,
-  });
-
-  // mount the component instance in the body element
-  const container = document.body;
-  container.appendChild(componentInstance);
-
-  return componentInstance;
+  customElements.define('chat-bot', ChatBot)  
 }
-
+createComponent()
 
