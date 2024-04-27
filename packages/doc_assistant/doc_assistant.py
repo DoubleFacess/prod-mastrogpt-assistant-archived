74d12648dda3bc9c1091e368c7aa180f1950e440
Tuo Nome <double.facessss@gmail.com>
2024-04-27 19:46:25 +0100

    fixing package and action

--- packages/doc_assistant/doc_assistant.py
+++
@@ -1,6 +0,0 @@
-def process_document(document):
-    # Qui puoi inserire la logica per elaborare il documento
-    # Ad esempio, puoi estrarre informazioni, eseguire operazioni o generare output
-    processed_document = "This is my processed document."
-    # Restituisci il risultato del processo del documento
-    return processed_document
--- packages/doc_assistant/main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-
-import doc_assistant
-
-# Esempio di utilizzo della funzione process_document
-document = "Questo è un documento di prova"
-processed_document = doc_assistant.process_document(document)
-print(processed_document)
